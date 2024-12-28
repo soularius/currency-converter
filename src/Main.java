f28dfc2789eaa3dce9c5e96959382c4a3c518424
Yined Molina <yined.molina@gmail.com>
2024-12-28 06:06:03 -0500

    Clase principal

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import Application.MenuSystem;
+import HTTPRest.HttpRequestExchangeRate;
+import HTTPRest.HttpResponseExchangeRate;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        MenuSystem menuSystem = new MenuSystem();
+        menuSystem.processMenuOption();
+    }
+}
